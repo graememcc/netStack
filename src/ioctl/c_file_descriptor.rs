b8496d337eb676b61ee3a88a17c77fa12e374853
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2014-10-14 21:54:43 +0100

    Implement Reader trait on c_file_descriptor

--- src/ioctl/c_file_descriptor.rs
+++ src/ioctl/c_file_descriptor.rs
@@ -1,5 +1,6 @@
-use super::libc::{c_int, close};
+use super::libc::{c_int, close, read, c_void, size_t};
 use std::fmt;
+use std::io::{Reader, IoResult, IoError, standard_error, EndOfFile};
 
 
 pub struct CFileDescriptor {
@@ -10,6 +11,35 @@ pub struct CFileDescriptor {
 impl CFileDescriptor {
     pub fn new(fd: c_int) -> CFileDescriptor {
         CFileDescriptor {fd: fd}
+    }
+}
+
+
+impl Reader for CFileDescriptor {
+    fn read(&mut self, buf: &mut [u8]) -> IoResult<uint> {
+        let buf_len = buf.len() as i64;
+        let buf_ptr = buf.as_ptr();
+
+        unsafe {
+            let bytes_read =  read(self.fd, buf_ptr as *mut c_void, buf_len as size_t);
+
+            match bytes_read {
+                0 => Err(standard_error(EndOfFile)),
+
+                -1 => Err(IoError::last_error()),
+
+                n @ _  => {
+                    if n > buf_len {
+                        // If C has screwed us by reading more than the buffer length, then we're in a bad spot. Our
+                        // return address might have been overwritten on the stack, as buf would have been in our
+                        // caller's stack frame.
+                        fail!("Buffer tainted by C read. Unsafe to continue")
+                    }
+
+                    Ok(n as uint)
+                }
+            }
+        }
     }
 }
 

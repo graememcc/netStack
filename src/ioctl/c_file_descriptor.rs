9ccaa1946d35a3003ff3fe44a25eae1b24444713
Graeme McCutcheon <graememcc_firefox@graeme-online.co.uk>
2014-10-16 16:12:29 +0100

    Add a wrapper for C file descriptors

---
+++ src/ioctl/c_file_descriptor.rs
@@ -0,0 +1,36 @@
+extern crate libc;
+use self::libc::{c_int, close};
+use std::fmt;
+
+
+pub struct CFileDescriptor {
+    fd: c_int
+}
+
+
+impl CFileDescriptor {
+    pub fn new(fd: c_int) -> CFileDescriptor {
+        CFileDescriptor {fd: fd}
+    }
+}
+
+
+impl Drop for CFileDescriptor {
+    fn drop(&mut self) {
+        let result = unsafe {
+            close(self.fd)
+        };
+
+        // There isn't much we can do if this failed
+        if result != 0 {
+            println!("Warning: failed to close file descriptor when dropping");
+        }
+    }
+}
+
+
+impl fmt::Show for CFileDescriptor {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<C File Descriptor {}>", self.fd)
+    }
+}
